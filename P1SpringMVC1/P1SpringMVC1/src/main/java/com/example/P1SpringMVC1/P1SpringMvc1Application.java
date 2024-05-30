a043ec84fbb237b4c7ce1bbc76ff66ca71f96751
Akhil Bille <akhilgowd829@gmail.com>
2024-05-29 21:35:01 -0400

    Spring MVC Project1 Inital Commit

---
+++ P1SpringMVC1/P1SpringMVC1/src/main/java/com/example/P1SpringMVC1/P1SpringMvc1Application.java
@@ -0,0 +1,13 @@
+package com.example.P1SpringMVC1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1SpringMvc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1SpringMvc1Application.class, args);
+	}
+
+}
---
+++ P1SpringMVC1/P1SpringMVC1/src/test/java/com/example/P1SpringMVC1/P1SpringMvc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.P1SpringMVC1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P1SpringMvc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
